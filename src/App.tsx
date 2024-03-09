7ea4fbc2791d031775a2cc995e02912518cc147c
Komandr33 <and.kom2017@gmail.com>
2024-03-09 23:14:50 +0300

    added Card in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import './App.css';
 import styled from 'styled-components';
 
-
 function App() {
     return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
+      <div className="App">
+        <Card>
+
+        </Card>
+
+      </div>
     );
 }
 
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+const Card = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 300px;
+  height: 350px;
+  border-radius: 15px;
+  background-color: rgb(255, 255, 255);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+`
+
+
