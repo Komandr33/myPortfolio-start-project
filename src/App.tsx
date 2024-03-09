ce245ab297f34269ef6ad13893d5af06c3bf9b04
Komandr33 <and.kom2017@gmail.com>
2024-03-09 23:16:36 +0300

    added Images and Title in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
 import styled from 'styled-components';
+import img1 from './images/rectangle.png';
 
 function App() {
     return (
       <div className="App">
         <Card>
-
+          <Images src={img1} alt={''}></Images>
+          <div style={{margin: '20px'}}>
+            <Title>Headline</Title>
+          </div>
         </Card>
 
       </div>
@@ -23,5 +27,17 @@ const Card = styled.div`
   background-color: rgb(255, 255, 255);
   box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
 `
+
+const Images = styled.img`
+  margin: 10px;
+  padding: 0;
+`
+
+const Title = styled.h1`
+  font-size: 16px;
+  letter-spacing: 0;
+  font-weight: 700;
+  color: black;
+`;
 
 
