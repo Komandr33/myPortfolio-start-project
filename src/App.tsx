6c97967db0b6689f05247e9e8d5bbf82a57e17a2
Komandr33 <and.kom2017@gmail.com>
2024-03-09 23:18:05 +0300

    added Button in App

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ function App() {
           <div style={{margin: '20px'}}>
             <Title>Headline</Title>
             <Content>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Content>
+            <Button>See more</Button>
+            <Button opasity >Save</Button>
           </div>
         </Card>
 
@@ -49,4 +51,17 @@ const Content = styled.p`
   margin: 19px 0 19px 0;
 `;
 
+type ButtonPropstype = {
+  opasity?: boolean
+}
 
+const Button = styled.button<ButtonPropstype>`
+  box-shadow: none;
+  width: 86px;
+  height: 30px;
+  border: 2px solid ${props => !props.opasity ? '#4E71FE' : ''};
+  border-radius: 5px;
+  margin-right: 12px;
+  color: ${props => props.opasity ? '#4E71FE' : 'white'};
+  background-color: ${props => props.opasity ? 'white' : '#4E71FE'};
+`;
